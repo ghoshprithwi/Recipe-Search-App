196526d9e21672c7b582309799c15d6658de52ca
PRITHWIRAJ <prithwiraj10@gmail.com>
2021-06-29 15:42:32 +0530

    added dependency, deployed to netlify

--- src/App.js
+++ src/App.js
@@ -21,15 +21,15 @@ class App extends Component {
         const recipeResults = await api_call.json()
         this.setState({ recipes: recipeResults.results });
     }
-    componentDidMount = () => {
-        const json = localStorage.getItem("recipes");
-        const recipes = JSON.parse(json);
-        this.setState({ recipes });
-    }
-    componentDidUpdate = () => {
-        const recipes = JSON.stringify(this.state.recipes);
-        localStorage.setItem("recipes", recipes);
-    }
+    // componentDidMount = () => {
+    //     const json = localStorage.getItem("recipes");
+    //     const recipes = JSON.parse(json);
+    //     this.setState({ recipes });
+    // }
+    // componentDidUpdate = () => {
+    //     const recipes = JSON.stringify(this.state.recipes);
+    //     localStorage.setItem("recipes", recipes);
+    // }
     render() {
         return (
             <div className="App">
